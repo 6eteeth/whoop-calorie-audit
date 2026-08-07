c5a6c85194038804f7a9ed12f88efc9a4a6775cf
6eteeth <zsoard@gmail.com>
2026-08-07 12:48:43 -0700

    split app vendor bundles

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          charts: ['chart.js'],
+          supabase: ['@supabase/supabase-js'],
+        },
+      },
+    },
+  },
 })
